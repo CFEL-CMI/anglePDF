dda05559d05891da69ab8a10de2476b709b09929
Nidin <nidin1996@gmail.com>
2022-02-14 11:53:38 +0100

    add another file in demo.py

--- anglePDF/demo.py
+++ anglePDF/demo.py
@@ -3,3 +3,4 @@ from pdf import AnglePDF
 trial1 = AnglePDF('fh95-1D-0.85-1000')
 trial2 = AnglePDF('fh95-1D-0.80-1000')
 trial3 = AnglePDF('unk-3D-0.73-1000')
+trial4 = AnglePDF('fh95-1D-0.97-100000')
